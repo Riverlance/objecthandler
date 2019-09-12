0aec6317eb193dbac2b6d4ccd42a2da2974ae397
Riverlance <biel94moraes@msn.com>
2019-09-12 10:26:40 -0300

    Updated header comment

--- src/graphic_mesh.cpp
+++ src/graphic_mesh.cpp
@@ -1,5 +1,5 @@
 /**
- * Object Handler - A 3D Model Viewer for .obj file extension.
+ * Object Handler - A 3D Model Viewer.
  * Copyright (C) 2019  Gabriel Moraes de Oliveira <biel94moraes@msn.com>
  */
 
